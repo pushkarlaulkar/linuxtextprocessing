32367f39d7d3ca5f8d5177fb287f6ee0fef27b6f
Pushkar Laulkar <pushkar.laulkar@yahoo.com>
2016-11-28 00:00:32 +0400

    Create grep1.sh

---
+++ Sed_Grep_Command/grep1.sh
@@ -0,0 +1,2 @@
+#This program outputs only those lines that contain the word 'the'. The search should be case sensitive. Here -w is used so that we get only the word "the" and not the words that contain the substring "the".
+grep -w "the"
